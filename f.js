e0db7349100c9e78783b93911db898995f882b6e
Muqeet-coder <rahilashahzad.7@gmail.com>
2024-08-05 19:42:58 +0500

    Add files via upload

---
+++ f.js
@@ -0,0 +1,18 @@
+let box = document.querySelector(".box");
+let resetbtn = document.querySelector(".resetbtn")
+
+const turno = true;
+
+let winpattens ={ 
+    [0,1,2] 
+    [0,3,6]
+    [0,4,8]
+    [1,4,7]
+    [2,5,8]
+    [2,4,6]
+    [3,4,5]
+    [6,7,8]
+};
+boxe.forEach((box) => {
+    box.addeventlisner("click")
+});
